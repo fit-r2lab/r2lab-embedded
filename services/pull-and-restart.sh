e239f2849435f69a8c8d6461445aa40bf5d91232
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2018-01-31 11:28:56 +0100

    updated and renamed pull-and-restart.sh

--- services/pull-and-restart.sh
+++ services/pull-and-restart.sh
@@ -5,19 +5,22 @@
 
 # xxx the django secret key and debug mode needs to be tweaked !
 
-LOG=/var/log/restart-website.log
+COMMAND=$(basename $0 .sh)
+LOG=/var/log/$COMMAND.log
 
 #### depending on which host:
 case $(hostname) in
     faraday*)
-	GIT_REPOS="/root/r2lab"
+	GIT_REPOS="/root/r2lab-embedded"
 	;;
     r2lab*)
-	GIT_REPOS="/root/r2lab /root/r2lab.inria.fr /root/r2lab.inria.fr-raw"
+	GIT_REPOS="/root/r2lab-embedded /root/r2lab-sidecar /root/r2lab.inria.fr /root/r2lab.inria.fr-raw"
 	;;
     *)
 	echo Unknown host $(hostname); exit 1;;
 esac
+
+echo "==================== $COMMAND starting at $(date)" >> $LOG
 
 #### updates the contents of selected git repos
 for git_repo in $GIT_REPOS; do
@@ -31,10 +34,12 @@ cd
 #### depending on which host:
 case $(hostname) in
     faraday*)
+        pip3 install -U rhubarbe
 	systemctl restart monitor
 	systemctl restart monitorphones
 	;;
     r2lab*)
+        pip3 install -U rhubarbe
 	make -C /root/r2lab.inria.fr publish >> $LOG 2>&1
 	make -C /root/r2lab.inria.fr-raw publish >> $LOG 2>&1
 	systemctl restart sidecar
