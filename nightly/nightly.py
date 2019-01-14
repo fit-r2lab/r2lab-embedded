b6440bccea0abe3e4673b2f61e5a49232a300b6c
parmentelat <thierry.parmentelat@inria.fr>
2019-01-14 20:53:41 +0100

    nightly: pylint'ed, use f-strings also shows how many nodes were scanned and how many issues were found no table if no issue

--- nightly/nightly.py
+++ nightly/nightly.py
@@ -21,6 +21,8 @@ Performed checks on all nodes:
 
 """
 
+# pylint: disable=c0111, r0201
+
 import sys
 import os
 import time
@@ -40,7 +42,8 @@ from rhubarbe.display import Display
 from rhubarbe.main import check_reservation, no_reservation
 from rhubarbe.node import Node
 from rhubarbe.leases import Leases
-from rhubarbe.selector import Selector, add_selector_arguments, selected_selector, MisformedRange
+from rhubarbe.selector import (
+    Selector, add_selector_arguments, selected_selector, MisformedRange)
 from rhubarbe.imageloader import ImageLoader
 from rhubarbe.ssh import SshProxy as SshWaiter
 
@@ -48,17 +51,15 @@ from nightmail import complete_html, send_email
 
 
 # global - need to be configurable ?
-nightly_slice = "inria_r2lab.nightly"
-email_from = "nightly@faraday.inria.fr"
-email_to = [
-    "fit-r2lab-dev@inria.fr",
-]
+NIGHTLY_SLICE = "inria_r2lab.nightly"
+EMAIL_FROM = "nightly@faraday.inria.fr"
+EMAIL_TO = ["fit-r2lab-dev@inria.fr"]
 
 
 # each image is defined by a tuple
 #  0: image name (for rload)
 #  1: strings to expect in /etc/rhubarbe-image (any of these means it's OK)
-images_to_check = [
+IMAGES_TO_CHECK = [
     ("ubuntu", ["ubuntu-16.04", "u16.04"]),
     ("fedora", ["fedora-27"]),
 ]
@@ -76,23 +77,23 @@ class Reason(IntEnum):
     def mail_column(self):
         # the outgoing mail comes with 3 columns
         # return 0 1 or 2 depending on the outgoing column
-        return 0 if self.value <= 3 \
-            else 1 if self.value <= 5 \
-            else 2
+        return (0 if self.value <= 3                   # pylint: disable=w0143
+                else 1 if self.value <= 5              # pylint: disable=w0143
+                else 2)
 
 # not sure how progressbar would behave in unattended mode
 # that would meand no terminal and so no width to display a progressbar..
 
 
 class NoProgressBarDisplay(Display):
-    def dispatch_ip_percent_hook(self, *args):
+    def dispatch_ip_percent_hook(self, *_ignore):
         print('.', end='', flush=True)
 
-    def dispatch_ip_tick_hook(self, *args):
+    def dispatch_ip_tick_hook(self, *_ignore):
         print('.', end='', flush=True)
 
 
-class Nightly:
+class Nightly:                                         # pylint: disable=r0902
 
     def __init__(self, selector, verbose=False):
         # work selector; will remove nodes as they fail
@@ -128,24 +129,23 @@ class Nightly:
         """
         self.selector.add_or_delete(node.id, add_if_true=False)
         self.failures[node.id] = reason
-        # xxx ok this may be a be a bit fragile, but given that sidecar_client
+        # ok, this may be a be a bit fragile, but given that sidecar_client
         # is not properly installed...
         unavailable_script = Path.home() / "r2lab-python/examples/unavailable.py"
         if not unavailable_script.exists():
-            print("Cannot locate unavailable script {} - skipping"
-                  .format(unavailable_script))
+            print(f"Cannot locate unavailable script {unavailable_script}"
+                  f" - skipping")
             return
-        command = "{command} {number}"\
-                  .format(command=unavailable_script, number=node.id)
+        command = f"{unavailable_script} {node.id}"
         os.system(command)
 
     def global_send_action(self, mode):
         delay = 5.
-        self.verbose_msg("delay={}".format(delay))
+        self.verbose_msg(f"delay={delay}")
         nodes = {Node(x, self.bus) for x in self.selector.cmc_names()}
         actions = (node.send_action(message=mode, check=True, check_delay=delay)
                    for node in nodes)
-        result = asyncio.get_event_loop().run_until_complete(
+        _result = asyncio.get_event_loop().run_until_complete(
             asyncio.gather(*actions)
         )
         reason = Reason.WONT_TURN_OFF if mode == 'on' \
@@ -153,8 +153,7 @@ class Nightly:
             else Reason.WONT_RESET
         for node in nodes:
             if node.action:
-                print("{}: {} OK"
-                      .format(node.control_hostname(), mode))
+                print(f"{node.control_hostname()}: {mode} OK")
             else:
                 self.mark_and_exclude(node, reason)
 
@@ -165,16 +164,15 @@ class Nightly:
         actual_image = the_imagesrepo.locate_image(
             image_name, look_in_global=True)
         if not actual_image:
-            print("Image file {} not found - emergency exit"
-                  .format(image_name))
+            print(f"Image file {image_name} not found - emergency exit")
             exit(1)
 
         # load image
         nodes = {Node(x, self.bus) for x in self.selector.cmc_names()}
         display = NoProgressBarDisplay(nodes, self.bus)
-        self.verbose_msg("image={}".format(actual_image))
-        self.verbose_msg("bandwidth={}".format(self.bandwidth))
-        self.verbose_msg("timeout={}".format(self.load_timeout))
+        self.verbose_msg(f"image={actual_image}")
+        self.verbose_msg(f"bandwidth={self.bandwidth}")
+        self.verbose_msg(f"timeout={self.load_timeout}")
         loader = ImageLoader(nodes, image=actual_image,
                              bandwidth=self.bandwidth,
                              message_bus=self.bus, display=display)
@@ -184,8 +182,7 @@ class Nightly:
         # wait for nodes to be ssh-reachable
         nodes = {Node(x, self.bus) for x in self.selector.cmc_names()}
         display = NoProgressBarDisplay(nodes, self.bus)
-        print("Waiting for {} nodes (timeout={})"
-              .format(len(nodes), self.wait_timeout))
+        print(f"Waiting for {len(nodes)} nodes (timeout={self.wait_timeout})")
         sshs = [SshWaiter(node, verbose=self.verbose) for node in nodes]
         jobs = [Job(ssh.wait_for(self.backoff), critical=False)
                 for ssh in sshs]
@@ -195,24 +192,25 @@ class Nightly:
                               critical=False,
                               timeout=self.wait_timeout)
         if not scheduler.run():
-            self.verbose and scheduler.debrief()
+            self.verbose and scheduler.debrief()        # pylint: disable=w0106
         # exclude nodes that have not behaved
         for node, job in zip(nodes, jobs):
-            print("node-> {} job -> done={} exc={}"
-                  .format(node.id, job.is_done(), job.raised_exception()))
+            print(f"node-> {node.id} job -> done={job.is_done()}"
+                  f" exc={job.raised_exception()}")
 
             if job.raised_exception():
                 self.mark_and_exclude(node, Reason.WONT_SSH)
 
-    def global_check_image(self, image, check_strings):
+    def global_check_image(self, _image, check_strings):
         # on the remaining nodes: check image marker
         nodes = {Node(x, self.bus) for x in self.selector.cmc_names()}
         display = NoProgressBarDisplay(nodes, self.bus)
-        print("Checking {} nodes against {} in /etc/rhubarbe-image"
-              .format(len(nodes), check_strings))
+        print(f"Checking {len(nodes)} nodes"
+              f" against {check_strings} in /etc/rhubarbe-image")
 
-        check_command = "tail -1 /etc/rhubarbe-image | egrep -q '{}'"\
-                        .format("|".join(check_strings))
+        grep_pattern = "|".join(check_strings)
+        check_command = (
+            f"tail -1 /etc/rhubarbe-image | egrep -q '{grep_pattern}'")
         jobs = [
             SshJob(node=SshNode(hostname=node.control_hostname(), keys=[]),
                    command=check_command,
@@ -225,16 +223,17 @@ class Nightly:
                               critical=False,
                               timeout=self.wait_timeout)
         if not scheduler.run():
-            self.verbose and scheduler.debrief()
+            self.verbose and scheduler.debrief()        # pylint: disable=w0106
         # exclude nodes that have not behaved
         for node, job in zip(nodes, jobs):
             if not job.is_done() or job.raised_exception():
                 self.verbose_msg(
-                    "something went badly wrong with {}".format(node))
+                    f"something went badly wrong with {node}")
                 self.mark_and_exclude(node, Reason.CANT_CHECK_IMAGE)
                 continue
             if not job.result() == 0:
-                self.verbose_msg("Wrong image found on {}".format(node))
+                self.verbose_msg(
+                    f"Wrong image found on {node}")
                 self.mark_and_exclude(node, Reason.DID_NOT_LOAD)
                 continue
 
@@ -248,12 +247,11 @@ class Nightly:
         leases = Leases(message_bus=self.bus)
         if no_reservation(leases):
             return None
-        elif check_reservation(leases, root_allowed=False,
-                               verbose=None if not self.verbose else True,
-                               login=nightly_slice):
+        if check_reservation(leases, root_allowed=False,
+                             verbose=None if not self.verbose else True,
+                             login=NIGHTLY_SLICE):
             return True
-        else:
-            return False
+        return False
 
     def all_off(self):
         if self.verbose:
@@ -261,7 +259,7 @@ class Nightly:
             return
         command = "rhubarbe bye"
         for host in self.all_names:
-            command += " {}".format(host)
+            command += f" {host}"
         command += "> /var/log/all-off.log"
         os.system(command)
 
@@ -273,14 +271,16 @@ class Nightly:
         # we have the lease, let's get down to business
         # skip this test in verbose mode
         print(40*'=')
-        print("Nightly check - starting at {}"
-              .format(time.strftime("%Y-%m-%d@%H:%M:%S",
-                                    time.localtime(time.time()))))
+        showtime = time.strftime(
+            "%Y-%m-%d@%H:%M:%S",
+            time.localtime(time.time()))
+        print(f"Nightly check - starting at {showtime}")
 
         print(40*'=')
 
-        self.verbose_msg("focus is {}" .format(
-            " ".join(self.selector.node_names())))
+        self.verbose_msg(f"focus is {self.all_names}")
+
+        number_nodes = len(self.all_names)
 
         current_owner = self.current_owner()
 
@@ -301,8 +301,8 @@ class Nightly:
         else:
             print("nightly in verbose mode won't check on and off and reset")
 
-        images_expected = images_to_check if not self.verbose \
-            else images_to_check[:1]
+        images_expected = IMAGES_TO_CHECK if not self.verbose \
+            else IMAGES_TO_CHECK[:1]
 
         for image, check_strings in images_expected:
             if not self.verbose:
@@ -312,18 +312,18 @@ class Nightly:
             self.global_wait_ssh()
             self.global_check_image(image, check_strings)
 
-        html = complete_html(self.failures)
+        html = complete_html(self.all_names, self.failures)
         if self.failures:
-            plural = '' if len(self.failures) == 1 else ''
-            subject = "R2lab nightly reports {} issue{}"\
-                      .format(len(self.failures), plural)
+            subject = (f"R2lab nightly on {number_nodes} node(s)"
+                       f" - found {len(self.failures)} issue(s)")
         else:
-            subject = "R2lab nightly - everything is fine"
+            subject = (f"R2lab nightly on {number_nodes} node(s)s "
+                       f"- everything is fine")
 
         if self.verbose:
             print("verbose mode: skip sending mail")
         else:
-            send_email(email_from, email_to, subject, html)
+            send_email(EMAIL_FROM, EMAIL_TO, subject, html)
 
         self.all_off()
 
@@ -332,13 +332,13 @@ class Nightly:
 
 
 ####################
-usage = """
+USAGE = """
 Run nightly check procedure on R2lab
 """
 
 
 def main(*argv):
-    parser = ArgumentParser(usage=usage)
+    parser = ArgumentParser(usage=USAGE)
     parser.add_argument("-v", "--verbose", action='store_true', default=False,
                         help="for testing purposes only")
     add_selector_arguments(parser)
@@ -354,6 +354,6 @@ def main(*argv):
 if __name__ == '__main__':
     try:
         exit(main(*sys.argv[1:]))
-    except MisformedRange as e:
-        print("ERROR: ", e)
+    except MisformedRange as exc:
+        print("ERROR: ", exc)
         exit(1)
--- nightly/nightmail.py
+++ nightly/nightmail.py
@@ -1,5 +1,6 @@
-# would have been much nicer with 3.6 f-strings but that's not
-# yet available on faraday that runs ubuntu-16.04
+"""
+helper functions to prepare and send a summary mail once nightly is done
+"""
 
 from datetime import datetime
 import smtplib
@@ -10,60 +11,93 @@ def font_color_tick(column, failed_column):
 
     return (
         # all is fine, show a green dot
-        ('32px Arial, Tahoma, Sans-serif' , '#42c944' , '&#8226;') if column < failed_column
+        ('32px Arial, Tahoma, Sans-serif', '#42c944', '&#8226;')
+        if column < failed_column
         # otherwise, a red cross
-        else ('18px Arial, Tahoma, Sans-serif',  'red', '&#215;') if column == failed_column
-        else ('18px Arial, Tahoma, Sans-serif',  'gray', '&#65110;'))
+        else ('18px Arial, Tahoma, Sans-serif', 'red', '&#215;')
+        if column == failed_column
+        else ('18px Arial, Tahoma, Sans-serif', 'gray', '&#65110;'))
 
 
-def summary_table(failures):
+def header_line(nodenames, failures):
     """
-    based on the failures structure
-    that maps node ids to a failure reason
-    produce a summary mail
+    Returns a HTML fragment with an overview of the results
     """
+    return (f"<p>On [DATE]"
+            f"<br/>Report on {len(nodenames)} nodes"
+            f"<br/>Detected {len(failures)} issues.</p>")
+
+def summary_table(nodenames, failures):
+    """
+    based on the failures structure that maps node ids to a failure reason
+    produce a table where eavh failed node is attached a failure reason
+    """
+
+    if not failures:
+        return f"<p>All {len(nodenames)} nodes were found to be fine.</p>"
 
     header = """<tr>
 <td style="width: 40px; text-align: center;">
  <h5><span style="background:#f0ad4e; color:#fff; padding:4px; border-radius: 5px;">[DATE]
  </span></h5></td>
 <td style="font:11px Arial, Tahoma, Sans-serif; width: 40px; text-align: center;">
- <img src="http://r2lab.inria.fr/assets/img/nightly-power.png" style="width:25px;height:25px;" />&nbsp;start&nbsp;</td>
+ <img src="http://r2lab.inria.fr/assets/img/nightly-power.png"
+  style="width:25px;height:25px;" />&nbsp;start&nbsp;</td>
 <td style="font:11px Arial, Tahoma, Sans-serif; width: 40px; text-align: center;">
- <img src="http://r2lab.inria.fr/assets/img/nightly-share.png" style="width:25px;height:25px;" />load</td>
+ <img src="http://r2lab.inria.fr/assets/img/nightly-share.png"
+ style="width:25px;height:25px;" />load</td>
 <td style="font:11px Arial, Tahoma, Sans-serif; width: 40px; text-align: center;">
- <img src="http://r2lab.inria.fr/assets/img/nightly-zombie.png" style="width:25px;height:25px;" />zombie</td>
+ <img src="http://r2lab.inria.fr/assets/img/nightly-zombie.png"
+ style="width:25px;height:25px;" />zombie</td>
 <td>&nbsp;&nbsp;</td>
 </tr>"""
 
     result = header
-    for id in sorted(failures):
-        reason = failures[id]
+    for node_id in sorted(failures):
+        reason = failures[node_id]
         # the node id badge
-        line = """<tr>
-<td style="font:15px helveticaneue, Arial, Tahoma, Sans-serif;">
- <span style="border-radius: 50%; border: 2px solid #525252; width: 30px; height: 30px; line-height: 30px; display: block; text-align: center;">
-  <span style="color: #525252;">{}
-</span></span></td>""".format(id)
-        # which column whould be outlined
+        line = f"""<tr>
+<td><span class="foo"><span class="bar">{node_id}</span></span></td>"""
+        # which column should be outlined
         failed_column = reason.mail_column()
         for column in range(3):
             font, color, tick = font_color_tick(column, failed_column)
-            line += '<td style="text-align: center; font:{font}; color:{color}">{tick}</td>'.format(**locals())
+            line += f"""<td style="text-align: center; font:{font}; color:{color}">{tick}</td>"""
         line += "</tr>"
         result += line
     return result
 
 
-def dummy_html():
+def html_skeleton():
+    """
+    The skeleton for the summary mail
+    """
     body = '''<!DOCTYPE html><html lang="en">
 <head>
  <meta charset="utf-8">
  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <style>
+td {
+    font:15px helveticaneue, Arial, Tahoma, Sans-serif;
+}
+td>span.foo {
+    border-radius: 50%;
+    border: 2px solid #525252;
+    width: 30px;
+    height: 30px;
+    line-height: 30px;
+    display: block;
+    text-align: center;
+}
+td>span.bar {
+    color: #525252;
+}
+ </style>
 </head>
 <body style="font:14px helveticaneue, Arial, Tahoma, Sans-serif; margin: 0;">
+[HEADER]
  <table style="padding: 10px;">
-  [CONTENTS]
+  [TABLE]
  </table>
 </body>
 </html>'''
@@ -71,19 +105,28 @@ def dummy_html():
 
 
 # entry points of interest start here
-def complete_html(failures):
+def complete_html(nodenames, failures):
+    """
+    The main entry point to the outside
+    Put it all together and create the mail body
+    """
     now = datetime.now()
     today = now.strftime("%d/%m/%Y")
-    template = dummy_html()
-    html = template.replace("[CONTENTS]",
-                            summary_table(failures))\
-                   .replace("[DATE]",
-                            today)
+    template = html_skeleton()
+    html = (template
+            .replace("[HEADER]",
+                     header_line(nodenames, failures))
+            .replace("[TABLE]",
+                     summary_table(nodenames, failures))
+            .replace("[DATE]",
+                     today))
     return html
 
 
 def send_email(sender, receiver, subject, content):
-    """ send email using python """
+    """
+    actually send email
+    """
     from email.mime.multipart import MIMEMultipart
     from email.mime.text import MIMEText
 
@@ -105,19 +148,20 @@ def send_email(sender, receiver, subject, content):
     msg.attach(body)
 
     # Send the message via local SMTP server.
-    s = smtplib.SMTP('localhost')
+    mailer = smtplib.SMTP('localhost')
     # sendmail function takes 3 arguments: sender's address, recipient's address
     # and message to send - here it is sent as one string.
-    s.sendmail(sender, receiver, msg.as_string())
-    s.quit()
+    mailer.sendmail(sender, receiver, msg.as_string())
+    mailer.quit()
 
 
 
 
 
-
-
-if __name__ == '__main__':
+def main():
+    """
+    manual unit test
+    """
 
     from nightly import Reason
 
@@ -126,6 +170,12 @@ if __name__ == '__main__':
         31 : Reason.WONT_SSH,
         37 : Reason.DID_NOT_LOAD,
     }
+    fake_nodenames = [f'fit{x:02d}' for x in range(1, 11)]
     with open('foo.html', 'w') as output:
-        output.write(complete_html(fake_failures))
+        output.write(complete_html(
+            fake_nodenames, fake_failures))
     print('see foo.html')
+
+
+if __name__ == '__main__':
+    main()
