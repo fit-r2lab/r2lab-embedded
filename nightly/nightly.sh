9cae79873b6a73ed76c46aceb3d77f92dd2fb8c4
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2022-03-20 11:21:52 +0100

    give up on crontab, use systemd timer to trigger the nightly

--- nightly/nightly.sh
+++
@@ -1,5 +0,0 @@
-# may pass along options like -v
-SHELL=/bin/sh
-HOME=/root
-PATH=/root/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin
-python3 /root/r2lab-embedded/nightly/nightly.py "$@" >> /var/log/nightly.log 2>&1
