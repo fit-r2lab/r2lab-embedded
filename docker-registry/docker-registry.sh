9a155223beb06e8fec65ade15e938b6d67038b88
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2022-10-16 14:52:29 +0200

    docker-registry startup script needs a change after docker-registry moves into r2lab-embedded

--- docker-registry/docker-registry.sh
+++ docker-registry/docker-registry.sh
@@ -6,10 +6,10 @@
 
 mkdir -p /var/lib/docker-registry
 
-podman run -d -p 443:443 \
-       --restart=always \
+podman run -d --restart=always \
+       -p 443:443 \
        -v /var/lib/docker-registry:/var/lib/registry \
-       -v /root/docker-registry/certs:/certs \
+       -v /root/r2lab-embedded/docker-registry/certs:/certs \
        -e REGISTRY_HTTP_ADDR=0.0.0.0:443 \
        -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/sopnode-registry.crt \
        -e REGISTRY_HTTP_TLS_KEY=/certs/sopnode-registry.key \
