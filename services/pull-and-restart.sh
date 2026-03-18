55a6b4a109819896b61b7c1a802b68fe0e9e8f67
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2026-03-18 09:03:23 +0100

    pull and restart script rewritten

--- services/pull-and-restart.sh
+++ services/pull-and-restart.sh
@@ -1,71 +1,91 @@
 #!/bin/bash
 ###
-# this utility script is cron'ed to run every morning at 6:00
-# on both r2lab and faraday (and distrait)
+# this utility script is run cyclically by systemd on
+# - r2lab
+# - r2labapi
+# -	faraday
+# - (and distrait)
 #
-
-# xxx the django secret key and debug mode needs to be tweaked !
 
 COMMAND=$(basename $0 .sh)
 LOG=/var/log/$COMMAND.log
 
-exec >> $LOG 2>&1
+
+#### updates the contents of a specific git repo
+function gtr() { git "$@" rev-parse --abbrev-ref --symbolic-full-name "@{u}"; }
+
+# follow blindly the remote branch, no merge
+# return 0 in case a change, 1 otherwise
+function git-upgrade() {
+	local repo="$1"; shift
+	local before=$(git -C "$repo" rev-parse HEAD)
+    git -C "$repo" fetch --all
+    git -C "$repo" reset --hard $(gtr -C "$repo")
+	local after=$(git -C "$repo" rev-parse HEAD)
+	[[ "$before" != "$after" ]]
+}
+
+# same kind of tools with pip
+function pip-upgrade() {
+    local pkg="$1"
+    local before=$(pip show "$pkg" 2>/dev/null | grep ^Version)
+    pip install --upgrade "$pkg" -q
+    local after=$(pip show "$pkg" 2>/dev/null | grep ^Version)
+    [[ "$before" != "$after" ]]
+}
 
 echo "==================== $COMMAND starting at $(date)"
 
 #### depending on which host:
 case $(hostname) in
     faraday*|distrait*)
-		GIT_REPOS="/root/r2lab-embedded"
+		if git-upgrade /root/r2lab-embedded; then
+			# expose changes to slices/users
+			runuser -u faraday -- git -C /home/faraday/r2lab-embedded fetch
+			runuser -u faraday -- git -C /home/faraday/r2lab-embedded reset --hard origin/master
+		fi
+
+		# note that a previous version would always restart these services
+		# also this was not done on distrait
+		if pip-upgrade rhubarbe; then
+			systemctl restart monitornodes
+			systemctl restart monitorphones
+			systemctl restart accountsmanager
+			# this service will soon disappear from rhubarbe - and thus from faraday
+			# but we keep it until deploying r2labapi
+			systemctl restart monitorleases || true
+		fi
+	;;
+    prod-r2labapi*|r2labapi*)
+		git-upgrade /root/r2lab-embedded
+
+		if git-upgrade /root/r2lab-api; then
+			cd /root/r2lab-api
+      		uv sync
+      		systemctl restart r2lab-api
+		fi
 	;;
     prod-r2lab*|r2lab*)
-		GIT_REPOS="/root/r2lab-embedded /root/r2lab.inria.fr /root/r2lab.inria.fr-raw"
+		git-upgrade /root/r2lab-embedded
+
+		git-upgrade r2lab.inria.fr; local r1=$?
+		git-upgrade r2lab.inria.fr-raw; local r2=$?
+		if (( r1 == 0|| r2 == 0)); then
+			make -C /root/r2lab.inria.fr publish
+			make -C /root/r2lab.inria.fr-raw publish
+			systemctl restart r2lab-django
+			systemctl restart nginx
+		fi
+
+		if pip-upgrade r2lab-sidecar; then
+			systemctl restart r2lab-sidecar
+		fi
+
+        pip install -U rhubarbe
 	;;
+
     *)
-		echo Unknown host $(hostname); exit 1;;
-esac
-
-#### updates the contents of selected git repos
-function gtr() { git "$@" rev-parse --abbrev-ref --symbolic-full-name "@{u}"; }
-# follow blindly
-function gfollow() {
-    git "$@" fetch --all
-    git "$@" reset --hard $(gtr "$@")
-}
-
-for git_repo in $GIT_REPOS; do
-    gfollow -C $git_repo
-done
-
-# also update the r2lab-embedded repo in /home/faraday for regular users
-case $(hostname) in
-	faraday*|distrait*)
-		runuser -u faraday -- git -C /home/faraday/r2lab-embedded fetch
-		runuser -u faraday -- git -C /home/faraday/r2lab-embedded reset --hard origin/master
-	;;
-esac
-
-cd
-
-#### depending on which host:
-case $(hostname) in
-	distrait*)
-        pip3 install -U rhubarbe 2> /dev/null
-	;;
-    faraday*)
-        pip3 install -U rhubarbe 2> /dev/null
-		systemctl restart monitornodes
-		systemctl restart monitorphones
-		systemctl restart monitorleases
-		systemctl restart accountsmanager
-	;;
-    prod-r2lab*|r2lab*)
-        pip3 install -U rhubarbe 2> /dev/null
-		pip3 install -U r2lab-sidecar 2> /dev/null
-		make -C /root/r2lab.inria.fr publish
-		make -C /root/r2lab.inria.fr-raw publish
-		systemctl restart r2lab-django
-		systemctl restart r2lab-sidecar
-		systemctl restart nginx
+		echo Unknown host $(hostname)
+		exit 1
 	;;
 esac
